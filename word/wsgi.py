e067b4eb75a5e18dc06b6f75a7e221cba172c7d8
icecola <fjl2401@163.com>
2020-07-17 11:27:09 +0800

    fix wsgi

--- word/wsgi.py
+++ word/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+os.environ.setdefault("ENV", "production")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'word.settings')
 
 application = get_wsgi_application()
